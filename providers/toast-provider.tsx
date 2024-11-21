3bcd977743b6f964bc2ecf34c95a3cd7276f7177
Coriano Harris <corianoharrispro@gmail.com>
2024-11-21 14:30:41 -0600

    added providers

---
+++ providers/toast-provider.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { ToastProvider as Provider } from "@/components/ui/toast"
+
+export function ToastProvider({ children }: { children: React.ReactNode })
+{
+    return <Provider>{children}</Provider>
+}
